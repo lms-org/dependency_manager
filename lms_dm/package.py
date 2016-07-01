208d615d0335d7b15c86a6911e6bb11ffce43ef7
phibedy <philipp.schmette@gmx.de>
2016-07-01 19:11:51 +0200

    refactoring, (doesn't work atm, still a lot of work)

---
+++ lms_dm/package.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+import package_manager
+
+class Package:
+        self.workingDir = "" #relative path, should be <.../dependency>
+        self.nameWithExtensions = ""#name with extension, for example lms:develop
+        self.name = "" #name of the package
+        self.url = ""#url to the "parent, for example at github
+
+############################################################
+##### static functions
+
+    def getPurePackageName(packageFull):
+        return packageFull.split(":")[0]
+
+
+    def isDirPackage(path):
+        packageFile = path+'/lms_package.json'
+        if not os.path.isfile(packageFile):
+            return False
+        return True
+
+    def getPackageNameFromPath(path):
+        packageFile = path+'/lms_package.json'
+        if not os.path.isfile(packageFile):
+            print('lms_package.json does not exist in: ' + package)
+            return;
+        packageData = parseJson(packageFile) #TODO error handling
+        self.name = packageData['name']  
+
+    def fromPath(path):
+        if not isDirPackage(path)
+            return #TODO error handling
+        p = Package(getPackageNameFromPath(path))
+        p.setPackageDir(path)
+        return p
+
+#############################################################
+#####
+
+    
+    def __init__(self,nameWithExtensions,workingDir):
+        self.nameWithExtensions = nameWithExtensions
+        self.name = getPurePackageName(nameWithExtensions)
+
+    def getDir(self):
+        return os.path.join(self.workingDir,self.name)
+
+    def installedGlobally(self):
+        return isPackageInstalledGlobally(self)
+
+    def installWithDependencies(self, ignoreGlobal=False):
+        #install or update the current package
+        self.installOrUpdate(ignoreGlobal)
+        #get all dependencies
+        dependencies = self.getPackageDependencies(dependencyDir+'/'+package)
+        print("DEPENDENCIES: {0}".format(dependencies))
+        for dependency in dependencies:
+            dependency.installWithDependencies(ignoreGlobal)
+
+
+    def installOrUpdate(self, ignoreGlobal=False):
+        #check if it is installed globally
+        if not ignoreGlobal and installedGlobally:
+            print(self.name + " already installed globally, you might have to update it manually")
+            return
+        #set the url
+        self.setUrl(package_manager.getPackageUrlFromName(package))
+            #check if a url was set
+        if self.url == None or len(self.url)==0:
+            print('Package not found: '+package)
+            sys.exit(1)
+
+        #TODO handle binaries
+
+        ###get current source
+        #create path
+        os.makedirs(workingDir, exist_ok=True)
+        myDir = self.getDir()
+        dirAbs = os.path.abspath(myDir)
+        if isGitUrl(packageUrl):
+            #check if folder already exists
+            print("mydir: " + myDir)
+            if os.path.isdir(myDir):
+                #pull the dir
+                p = subprocess.Popen(['git', 'pull'], cwd=myDir)
+                output, err = p.communicate()
+                if err is not None:
+                    print(output)
+                    print("pull failed")
+                    sys.exit(1)
+                print("pulled package")
+                #TODO error handling
+            else : 
+                #ret = subprocess.call(["git","clone",packageUrl, dir])
+                p = subprocess.Popen(['git', 'clone', packageUrl], cwd=dependencyDir)
+                output, err = p.communicate()
+                if err is not None:
+                    print(output)
+                    print("clone failed: "+ myDir)
+                    sys.exit(1)
+                print("cloned package")
+            print("LEN: {0}".format(len(packageNameParts)))
+            if len(packageNameParts) > 1:
+                print("checking out: "+packageNameParts[1])
+                p = subprocess.Popen(['git', 'checkout',packageNameParts[1]], cwd=myDir)
+                output, err = p.communicate()
+                if err is not None:
+                    print(output)
+                    print("can't checkout: "+packageNameParts[1])
+                    sys.exit(1)
+
+        elif isLocalFolder(packageUrl) :
+            print('handle local package: ' +packageName)
+            if not os.path.isabs(packageUrl):
+                packageUrl = os.path.abspath(packageUrl);
+            #create symlink
+            #check if symlink already exists TODO check if valid
+            if not os.path.exists(dirAbs):
+                os.symlink(packageUrl, dirAbs, True)
+            else:
+                print(dirAbs + ' already exists')
+        else :
+            print("no valid url-type given")
+            sys.exit(1)
+        
+        
+
+        
+
+    def isBinary(self):
+        #TODO
+        return False
+
+
+    def isSource(self):
+        #TODO
+        return False
+
+
+###############################################################
+#####Set functions
+    
+    def setUrl(self,url):
+        self.url = url;
+
+    def setPackageDir(self,packageDir):
+        self.packageDir = packageDir;
+
+    def isGitUrl(self):
+        if 'github.com' in self.url: #TODO
+            return True
+        return False
+
+    def isLocalFolder(self):
+        if os.path.isdir(self.url):
+            return True
+        return False
+
+    def isZipFile(self):
+        return False #TODO
+
+    def getPackageFilePath(self):
+        return os.path.join(self.packageDir,'lms_package.json')
+
+    def getPackageDependencies(self):
+        res = list()
+        packageFile = self.getPackageFilePath()
+        if not os.path.isdir(self.packageDir):
+            print('package does not exist: ' + self.getDir())
+            return
+        if not os.path.isfile(packageFile):
+            print('lms_package.json does not exist in: ' + self.getDir())
+            return
+        packageData = parseJson(packageFile) #TODO error handling
+        if 'dependencies' in packageData:
+            names = packageData['dependencies']
+            for tmp in names:
+                res.append(Package(tmp,self.workingDir)
+            return res
+        return res
+
+    #returns a list with all binaries that have to be linked
+    def getPackageTargets(self):
+        packageFilePath = self.getPackageFilePath()
+        json = parseJson(packageFilePath)
+        if 'targets' in json:
+            return json['targets']
+        targets = list()
+        targets.append(packageName)
+        return targets
+
+
+    def getPackageIncludes(packageDir, absPath=True):
+        packageFilePath = self.getPackageFilePath()
+        json = parseJson(packageFilePath)
+        if 'includes' in json:
+            includes = json['includes']
+        else:
+            includes = list()
+            includes.add('include')
+            
+        result = list()
+        for include in includes:
+            if absPath:
+                result.append(os.path.abspath(self.packageDir+'/'+include))
+            else:
+                result.append(include)
+        return result
---
+++ lms_dm/package_manager.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+
+def isPackageInstalledGlobally(package):
+    if os.path.isdir(os.path.join(getPackageManagerGlobalDependencyDir(),package.name)):
+        return True
+    return False
+
+def getDir():
+    return os.path.expanduser('~/.lms/lms_pm/')
+
+def getBinDir():
+    return os.path.join(getDir,"bin")
+
+def getLibDir():
+    return os.path.join(getDir,"lib")
+
+def getIncludeDir():
+    return os.path.join(getDir,"include")
+
+
+def getPackageUrlFromName(packageName):
+    for packageListPath in getPackageLists():
+        if not os.path.isfile(packageListPath):
+            print('packageFile does not exist: '+packageListPath)
+            continue
+        packagesData = parseJson(packageListPath)
+        if packageName in packagesData:
+            return packagesData[packageName]['path']
+
+######################################################################
+##### environment variables
+
+def checkEnvironmentVariable(key,value):
+   # isSet = False
+    #envString = '';
+    if key in os.environ:
+        envString = os.environ[key]
+        pathsplit = envString.split(':')
+        if value in pathsplit:
+            return True
+    return False
+
+def checkEnviromentVariables():
+    packageManagerDir = os.path.expanduser('~/.lms/package_manager/')
+    #http://stackoverflow.com/questions/5971635/setting-reading-up-environment-variables-in-python
+    #check PATH
+    #export PATH="$PATH:~/.lms/package_manager/bin"
+    pathDir = os.path.join(packageManagerDir,'bin')
+    cpathDir = os.path.join(packageManagerDir,'include')
+    ldpathDir = os.path.join(packageManagerDir,'lib')
+
+    anyMissing=False
+    missing = not checkEnvironmentVariable('PATH',pathDir)
+    if missing:
+        anyMissing = True
+        print("PATH NOT set correctly")
+    missing = not checkEnvironmentVariable('CPATH',cpathDir)
+    if missing:
+        anyMissing = True
+        print("CPATH NOT set correctly")
+    missing = not checkEnvironmentVariable('LD_LIBRARY_PATH',ldpathDir)
+    if missing:
+        anyMissing = True
+        print("LD_LIBRARY_PATH NOT set correctly")
+    if not anyMissing:
+        print("Everything seems to be set correctly")
+        return
+    
+    #Ask to add missing env variables
+    #TODO os check http://stackoverflow.com/questions/1854/python-what-os-am-i-running-on
+    #UNIX
+    profileFile = os.path.expanduser('~/.profile')
+    lmsbash = os.path.join(getPackageManagerDir(),'.bashrc')
+
+    #add the lmsbash file
+    if not os.path.isfile(lmsbash):
+        #write the lms bashfile
+        with open(lmsbash,'w') as file:
+            bashString ="""export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:{1}"
+export PATH="$PATH:{2}"
+export CPATH="$CPATH:{3}" """.format(os.path.join(getPackageManagerDir(),'lib'),os.path.join(getPackageManagerDir(),'bin'),os.path.join(getPackageManagerDir(),'include')) 
+            file.write(bashString)
+
+    #check if they were already added but not loaded
+    with open(profileFile,'r') as file:
+        fileRead = file.read()
+        if lmsbash in fileRead:
+            print("already added, you may have to relog/restart")
+            print("If it doesn't work please include bash file yourself (add . "+lmsbash+" to your bashrc or HOME/.profile or to any other file, you could also set the PATH,CPATH,LD_LIBRARY_PATH yourself")
+            return
+        else:
+            print("Missing environment variables")
+    #Ask the user if we should add them in .profile
+    autoEnvInstall = install_utils.query_yes_no("lpm could add the environment variables to your .profile", None)
+
+    if autoEnvInstall and os.path.isfile(profileFile):
+        #add it ot .profile
+        with open(profileFile,'a') as file:
+            print('you can add it to ~/.profile')
+            profileString ="""\n#automatically added by the lms package manager
+if [ -d "{0}" ] ; then
+. "{0}"
+fi
+""".format(lmsbash)
+            file.write(profileString)
+            print("call <exec bash> to reload changes (might not work, just relog/reboot)")
+                
+    else:
+        print('please include bash file yourself (add . "'+lmsbash+'" to your bashrc or HOME/.profile or to any other file, you could also set the PATH,CPATH,LD_LIBRARY_PATH yourself)')
+    #WIN TODO
+
+def printPackageList():
+    data = install_utils.getPackageList()
+    #TODO alphabetisch sortieren
+    for listFile in data:
+        print("file: "+listFile)
+        for package in data[listFile]:
+            print(package + "   " + data[listFile][package]['description'])
